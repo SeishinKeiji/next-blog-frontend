fea53f0297b1773311899e165aa10d2c9a41c22d
SeishinKeiji <ahmadxgani01@gmail.com>
2023-02-01 10:19:10 +0700

    chore: add apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useApollo } from "lib/apollo";
 import type { NextComponentType } from "next";
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
@@ -9,12 +11,14 @@ type CustomAppProps = AppProps & { Component: CustomNextComponent };
 
 export default function App({ Component, pageProps }: CustomAppProps) {
   const Layout: CustomNextComponent | typeof Fragment = Component.Layout ? Component.Layout : Fragment;
-
+  const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
